9a4fa1ea472957cee6ab6040943a04e6471a26e6
Ion Pascari <ipascari@isd-soft.com>
2019-12-04 20:03:54 +0200

    Add project skeleton

---
+++ src/main/java/com/evil/devreminder/DevReminderApplication.java
@@ -0,0 +1,13 @@
+package com.evil.devreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevReminderApplication.class, args);
+	}
+
+}
