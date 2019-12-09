2e27ca436617ae97788cbd93ecedf5dccfa38193
Ion Pascari <ipascari@isd-soft.com>
2019-12-09 09:27:50 +0200

    Remove the commandLineRunner

--- src/main/java/com/evil/devreminder/config/DevReminderApplication.java
+++ src/main/java/com/evil/devreminder/config/DevReminderApplication.java
@@ -1,8 +1,6 @@
 package com.evil.devreminder.config;
 
-import com.evil.devreminder.repository.NoteRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,13 +16,6 @@ public class DevReminderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevReminderApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner init(NoteRepository noteRepository) {
-        return (args) -> {
-            noteRepository.deleteAll();
-        };
     }
 
 	@Bean
